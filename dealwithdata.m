f4dc64a1175e8987c8580c47d75734518f2a2e64
Zhou Sijie <zhousijie@pku.edu.cn>
2017-08-10 22:27:12 +0800

    Drawmapper

---
+++ dealwithdata.m
@@ -0,0 +1,337 @@
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,1),9);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartOne');
+saveas(gca, 'PartOne','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,2),9);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwo');
+saveas(gca, 'PartTwo','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,3),9);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThree');
+saveas(gca, 'PartThree','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,4),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartFour');
+saveas(gca, 'PartFour','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,5),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartFive');
+saveas(gca, 'PartFive','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,6),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartSix');
+saveas(gca, 'PartSix','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,7),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartSeven');
+saveas(gca, 'PartSeven','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,8),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartEight');
+saveas(gca, 'PartEight','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,9),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartNinet');
+saveas(gca, 'PartNine','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,10),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTen');
+saveas(gca, 'PartTen','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,11),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartEleven');
+saveas(gca, 'PartEleven','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,12),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwelve');
+saveas(gca, 'PartTwelve','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,13),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirteen');
+saveas(gca, 'PartThirteen','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,14),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartForteen');
+saveas(gca, 'PartForteen','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,15),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartFifteen');
+saveas(gca, 'PartFifteen','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,16),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartSixteen');
+saveas(gca, 'PartSixteen','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,17),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartSeventeen');
+saveas(gca, 'PartSeventeen','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,18),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartEighteen');
+saveas(gca, 'PartEighteen','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,19),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartNineteen');
+saveas(gca, 'PartNineteen','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,20),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwenty');
+saveas(gca, 'PartTwenty','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,21),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwentyone');
+saveas(gca, 'PartTwentyone','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,22),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwentytwo');
+saveas(gca, 'PartTwentytwo','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,23),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwentythree');
+saveas(gca, 'PartTwentythree','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,24),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwentyfour');
+saveas(gca, 'PartTwentyfour','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,25),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwentyfive');
+saveas(gca, 'PartTwentyfive','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,26),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('ParTwentysix');
+saveas(gca, 'PartTwentysix','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,27),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwentyseven');
+saveas(gca, 'PartTwentyseven','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,28),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwentyeight');
+saveas(gca, 'PartTwentyeight','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,29),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartTwentynine');
+saveas(gca, 'PartTwentynine','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,30),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirty');
+saveas(gca, 'PartThirty','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,31),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtyone');
+saveas(gca, 'PartThirtyone','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,32),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtytwo');
+saveas(gca, 'PartThirtytwo','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,33),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtythree');
+saveas(gca, 'PartThirtythree','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,34),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtyfour');
+saveas(gca, 'PartThirtyfour','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,35),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtyfive');
+saveas(gca, 'PartThirtyfive','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,36),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtySix');
+saveas(gca, 'PartThirtySix','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,37),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtySeven');
+saveas(gca, 'PartThirtySeven','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,38),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtyEight');
+saveas(gca, 'PartThirtyEight','jpg');
+
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,39),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartThirtyNine');
+saveas(gca, 'PartThirtyNine','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,40),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartForty');
+saveas(gca, 'PartForty','jpg');
+
+hist(ConstrainedTwoAbnormalStartFarFromTrue(:,1),30);
+xlabel('layer rank');
+ylabel('Count number');
+title('PartOne');
+saveas(gca, 'PartOne','jpg');
+
+
+
+
+
+hist(VarName11,15);
+xlabel('layer rank');
+ylabel('Count number');
+title('L3down');
+saveas(gca, 'L3up','jpg');
+
+hist(VarName12,15);
+xlabel('layer rank');
+ylabel('Count number');
+title('L4down');
+saveas(gca, 'L4up','jpg');
+
+
+hist(VarName13,15);
+xlabel('layer rank');
+ylabel('Count number');
+title('L5down');
+saveas(gca, 'L5up','jpg');
+
+hist(VarName14,15);
+xlabel('layer rank');
+ylabel('Count number');
+title('L6down');
+saveas(gca, 'L6up','jpg');
+
+
+hist(VarName15,15);
+xlabel('layer rank');
+ylabel('Count number');
+title('L7down');
+saveas(gca, 'L7up','jpg');
+
+hist(VarName16,15);
+xlabel('layer rank');
+ylabel('Count number');
+title('L8down');
+saveas(gca, 'L8up','jpg');
+
+hist(E00,10);
+xlabel('log_e(\rho) / \Omega \cdot m,');
+ylabel('Count number');
+title('\rho in first layer');
+saveas(gca, 'rho 1','jpg');
+
+hist(E1,10);
+xlabel('log_e(\rho) / \Omega \cdot m,');
+ylabel('Count number');
+title('\rho in second layer');
+saveas(gca, 'rho 2','jpg');
+
+a=0;
+for i = 1:1000
+    rm = normrnd(3,1);
+    if(rm >0 & rm<6)
+        a = [a,rm];
+    else
+        i = i - 1;
+    end
+end
+hist(a);
+
+m =zeros(7);
+for i = 1:length(a)
+    m(1+floor(0.5+a(i))) = m(1+floor(0.5+a(i)))+1;
+end
+
+
+
+
+b = floor(a);
+
+
+y = [12,1,42,5,62,44,7,27,3];
